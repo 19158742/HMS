d8a4c6f3d6282e566db75e5f09b4c208d8f9d1cf
shraddha <x19158742@student.ncirl.ie>
2020-06-20 23:30:27 +0530

    Added webapi project

---
+++ HM_WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace HM_WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
