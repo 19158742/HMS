5ae19f0b8234b34c0dab326697cc12b4e02372c1
shraddha <x19158742@student.ncirl.ie>
2020-07-30 15:52:19 +0530

    Implemented download invoice PDF api

---
+++ HM_ClientApp/HotelMgmt/Models/BookRoomViewModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HotelMgmt.Models
+{
+    public class BookRoomViewModels
+    {
+        public int BookingId { get; set; }
+        public int RoomId { get; set; }
+        
+        [DisplayName("Room Type")]
+        public string RoomTpe { get; set; }
+        
+        [DisplayName("From")]
+        [DisplayFormat(ApplyFormatInEditMode =true,DataFormatString ="0:dd/MMM/yyyy")]
+        public DateTime FromDt { get; set; }
+        
+        [DisplayName("To")] 
+        public DateTime ToDt { get; set; }
+        
+        [DisplayName("Customer Name")]
+        public string CustomerName { get; set; }
+
+        [DisplayName("Total Amount")]
+        public double TotalAmt { get; set; }
+
+        [DisplayName("Transaction Type")]
+        public string TransactionType { get; set; }
+    }
+}
